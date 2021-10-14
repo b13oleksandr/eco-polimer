39bc71f198ded1fdb4ae1fba578c7f449128c230
Oleksandr <b13.oleksandr@gmail.com>
2021-10-14 10:38:32 +0300

    bigger height video & autoplay

---
+++ src/js/section-video.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const section = document.querySelector('.js-video-section')
+  const content = document.querySelector('.js-content-video')
+  const playButton = document.querySelector('.js-play-video-btn')
+  const videoWrap = document.querySelector('.js-video-wrap')
+  const video = document.querySelector('.js-video')
+
+  playButton.addEventListener('click', () => {
+    content.style.display = 'none'
+    videoWrap.style.display = 'flex'
+    video.play()
+  })
+
+  const options = {
+    root: null,
+    rootMargin: '-50% 0% -50% 0%',
+    threshold: 0,
+  };
+
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log('test')
+        playButton.click()
+      } else {
+        video.pause()
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+  if (section) {
+    observer.observe(section);
+  }
+})
